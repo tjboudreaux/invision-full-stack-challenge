368c6ef2fbbf6ffd19e0bfb8f72aa1128ca40eab
Travis Boudreaux <tjboudreaux@gmail.com>
2015-11-06 03:37:53 -0600

    Update expression and add tests

--- common/lib/expression.js
+++ common/lib/expression.js
@@ -1,15 +1,45 @@
 'use strict';
 
 /**
+ * A regular expression to match mathematical expression strings against.
+ * @type {RegExp}
+ */
+var expressionRegex = /^(\d*)([+-\\\/*])(\d*)=$/;
+
+/**
  * @description A list of valid operation types
  * @private
  * @type {string[]}
  */
 var validOperations = ['+', '-', '*', '/'];
 
-
+/**
+ *
+ * @param expressionString
+ * @constructor
+ */
 var Expression = function(expressionString) {
+    this.parseExpressionString(expressionString);
+};
 
+/**
+ * @description Parse a mathematical expression string.
+ * @param expressionString
+ */
+Expression.prototype.parseExpressionString = function(expressionString)
+{
+    //replace whitespace
+    expressionString = expressionString.replace(/\s*/g, '');
+
+    if (!expressionRegex.test(expressionString)) {
+        throw new TypeError(expressionString + ' is not a parsable mathematical expression.');
+    }
+
+    var matches = expressionRegex.exec(expressionString);
+
+    this.valueA = matches[1];
+    this.operator = matches[2];
+    this.valueB = matches[3];
 };
 
 /**
@@ -20,12 +50,12 @@ var Expression = function(expressionString) {
  */
 Expression.prototype.evaluate = function() {
 
-    if (validOperations.indexOf(operator) == -1) {
-        throw new TypeError('Operator "' + operator + '" is invalid');
+    if (validOperations.indexOf(this.operator) == -1) {
+        throw new TypeError('Operator "' + this.operator + '" is invalid');
     }
 
-    if (typeof valueA != Number ||
-        typeof valueA != Number
+    if (typeof this.valueA != Number ||
+        typeof this.valueB != Number
     ) {
         throw new TypeError ("Values must be a valid number");
     }
--- common/test/expression.spec.js
+++ common/test/expression.spec.js
@@ -1,11 +1,47 @@
 var expect     = require('expect.js'),
     expression = require('../lib/expression');
 
-describe('ExpressionValidator', function(){
+describe('Expression', function(){
 
-    it('should throw a type error for invalid operations', function(){
-        expect(expression.evaluate('a', '102', '105').to.throw.Exception(function(e){
+    it('should throw a type error for invalid expressions', function(){
+        expect(function() { return new expression('abcde=')}).to.throwError(function(e){
             expect(e).to.be.a(TypeError);
-        }));
+            expect(e.toString()).to.equal('TypeError: abcde= is not a parsable mathematical expression.');
+        });
+    });
+
+    it('should parse valid addition expressions properly', function(){
+        var exp = new expression('123+12345=');
+        expect(exp.valueA).to.equal('123');
+        expect(exp.valueB).to.equal('12345');
+        expect(exp.operator).to.equal('+');
+    });
+
+    it('should parse valid subtraction expressions properly', function(){
+        var exp = new expression('123-12345=');
+        expect(exp.valueA).to.equal('123');
+        expect(exp.valueB).to.equal('12345');
+        expect(exp.operator).to.equal('-');
+    });
+
+    it('should parse valid multiplication expressions properly', function(){
+        var exp = new expression('123*12345=');
+        expect(exp.valueA).to.equal('123');
+        expect(exp.valueB).to.equal('12345');
+        expect(exp.operator).to.equal('*');
+    });
+
+    it('should parse valid division expressions properly', function(){
+        var exp = new expression('123/12345=');
+        expect(exp.valueA).to.equal('123');
+        expect(exp.valueB).to.equal('12345');
+        expect(exp.operator).to.equal('/');
+    });
+
+    it('should parse expressions with whitespace properly', function(){
+        var exp = new expression('123 / 12345 =');
+        expect(exp.valueA).to.equal('123');
+        expect(exp.valueB).to.equal('12345');
+        expect(exp.operator).to.equal('/');
     });
 });
