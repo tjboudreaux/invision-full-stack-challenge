362a961f632772188950e142202d8233b93e7a94
Travis Boudreaux <tjboudreaux@gmail.com>
2015-11-08 02:18:09 -0600

    Updating build scripts
    - making all scripts executable
    - adding more details for debugging failed builds

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+
 #install common dependencies
+echo "Started common lib install"
 cd common
 npm install
+echo "Finished common lib install"
 
 #install consumer dependencies
+echo "Started consumer lib install"
 cd ../consumer
 npm install
+echo "Finished consumer lib install"
 
 #install producer dependencies
+echo "Started producer lib install"
 cd ../producer
 npm install
+echo "Finished producer lib install"
--- bin/travis-tests.sh
+++ bin/travis-tests.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
 #install common dependencies
+echo "Started common test suite"
 cd common
 npm test
+echo "Finished common test suite"
 
 #install consumer dependencies
+echo "Started consumer test suite"
 cd ../consumer
 npm test
+echo "Finished consumer test suite"
 
 #install producer dependencies
+echo "Started producer test suite"
 cd ../producer
 npm test
+echo "Finished producer test suite"
