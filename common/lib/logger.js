d6c5ac1f21ddfdadfc0fe625ed4f094f5e6a450c
Travis Boudreaux <tjboudreaux@gmail.com>
2015-11-09 08:06:56 -0600

    Update logging options

--- common/lib/logger.js
+++ common/lib/logger.js
@@ -3,8 +3,29 @@
 var winston = require('winston');
 
 var consoleTransport = new (winston.transports.Console)({timestamp:true, colorize:true, handleExceptions: false, humanReadableUnhandledException: false, prettyPrint: false, stringify:true});
-var producerLogFileTransport = new (winston.transports.File)({timestamp:true, filename: '../logs/producer.log', level:'debug', maxsize: 1024000, maxFiles: 10, handleExceptions: true,json: false, humanReadableUnhandledException: false, prettyPrint: false, stringify:true});
-var consumerLogFileTransport = new (winston.transports.File)({timestamp:true, filename: '../logs/consumer.log', level:'debug', maxsize: 1024000, maxFiles: 10, handleExceptions: true,json: false, humanReadableUnhandledException: false, prettyPrint: false, stringify:true});
+
+//create uniform options for producer and consumer, except filenames
+var fileOptions = {
+    tailable: true,
+    timestamp:true,
+    level:'debug',
+    maxsize: 10240000,
+    maxFiles: 10,
+    handleExceptions: true,
+    json: false,
+    humanReadableUnhandledException: false,
+    prettyPrint: false,
+    stringify:true
+};
+
+var producerOptions = JSON.parse(JSON.stringify(fileOptions));
+producerOptions.filename =  '../logs/producer.log';
+
+var consumerOptions = JSON.parse(JSON.stringify(fileOptions));
+consumerOptions.filename =  '../logs/consumer.log';
+
+var producerLogFileTransport = new (winston.transports.File)(producerOptions);
+var consumerLogFileTransport = new (winston.transports.File)(consumerOptions);
 
 winston.loggers.add('producer', {
     transports: [consoleTransport, producerLogFileTransport]
